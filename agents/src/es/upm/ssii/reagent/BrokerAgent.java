ac0a3eb77ef149e9993f5e0ff7474958384bc2bf
DanielVazquezm1 <alumno.daniel.vazquezp@gmail.com>
2026-05-22 13:44:54 +0200

    feat: Interacción básica Broker-DataSourcing

--- agents/src/es/upm/ssii/reagent/BrokerAgent.java
+++ agents/src/es/upm/ssii/reagent/BrokerAgent.java
@@ -1,5 +1,71 @@
 package es.upm.ssii.reagent;
 
-public class BrokerAgent {
-    // TODO
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class BrokerAgent extends Agent {
+    protected void setup() {
+        System.out.println("[" + getLocalName() + "]: Broker Orquestador activado.");
+
+        // Esperamos un INFORM como respuesta del InformationSourcingAgent.
+        MessageTemplate filtroMensajeViviendas = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+
+        CyclicBehaviour comportamiento = new CyclicBehaviour(this) {
+            public void action() {
+                ACLMessage listaViviendas = blockingReceive(filtroMensajeViviendas);
+
+                if (listaViviendas != null) {
+                    System.out.println("[Broker] Respuesta recibida del Recopilador");
+                    System.out.println("[Resultado del Filtrado JSON]:");
+                    imprimirLista(listaViviendas);
+                }
+            }
+        };
+        addBehaviour(comportamiento);
+
+        // Filtro estático.
+        FiltroVivienda filtroEstatico = new FiltroVivienda();
+        filtroEstatico.tipo = "Apartment";
+        filtroEstatico.habitacionesMin = 3;
+        filtroEstatico.tienePiscina = true;
+        String jsonFiltro = new Gson().toJson(filtroEstatico);
+
+        // Enviamos petición.
+        ACLMessage peticion = new ACLMessage(ACLMessage.REQUEST);
+        peticion.addReceiver(new AID("sourcing", AID.ISLOCALNAME));
+        peticion.setContent(jsonFiltro);
+
+        System.out.println("[Broker] Enviando Filtro Estático (Piso, >=3 habs, Piscina) a 'sourcing'...");
+        send(peticion);
+    }
+
+    private void imprimirLista(ACLMessage listaViviendas) {
+        try {
+            // Parseamos el texto y lo convertimos a un Array de JSON
+            JsonArray lista = JsonParser.parseString(listaViviendas.getContent()).getAsJsonArray();
+
+            for (int i = 0; i < lista.size(); i++) {
+
+                // Extraemos el objeto en la posición i.
+                JsonObject elemento = lista.get(i).getAsJsonObject();
+
+                // Sacamos los datos individuales.
+                String titulo = elemento.get("titulo").getAsString();
+                int precio = elemento.get("precio").getAsInt();
+
+                // Los imprimimos.
+                System.out.println("-> Casa " + i + ": " + titulo + " cuesta " + precio + "€");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al procesar la lista JSON: " + e.getMessage());
+        }
+    }
 }
